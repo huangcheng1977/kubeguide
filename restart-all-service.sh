1064a09cee225fa01e8dda77a2422a56cbe463f3
carvin_chen <carvin_chen@kingdee.com>
2017-05-24 11:29:45 +0800

    modify restart-all-service.sh

--- restart-all-service.sh
+++ restart-all-service.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/bash
+
 systemctl restart kube-apiserver
 systemctl restart kube-controller-manager
 systemctl restart kube-scheduler
-systemctl restart kubelet
-systemctl restart kube-proxy
+#systemctl restart kubelet
+# systemctl restart kube-proxy
