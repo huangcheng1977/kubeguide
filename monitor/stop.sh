920767b9bd85a8cc9d52ca79700e8388d3010f06
carvin_chen <carvin_chen@kingdee.com>
2017-05-24 20:30:48 +0800

    rm monitor

--- monitor/stop.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete -f heapster-service.yaml
-kubectl delete -f influxdb-service.yaml
-kubectl delete -f grafana-service.yaml
-kubectl delete -f influxdb-grafana-controller-v3.yaml
-kubectl delete -f heapster-controller-v1.1.0.yaml
