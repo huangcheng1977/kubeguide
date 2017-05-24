3ece9e4b1629bac4c41d7090c1feedc6d62fa17f
carvin_chen <carvin_chen@kingdee.com>
2017-05-24 14:11:52 +0800

    add clean.sh

---
+++ make-ca/clean.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+rm -f ca.*
+rm -f cs_client.*
+rm -f kubelet_client.*
+rm -f server.*
--- make-ca/make-ca.sh
+++ make-ca/make-ca.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+# apiserver
 openssl genrsa -out ca.key 2048
 openssl req -x509 -new -nodes -key ca.key -subj "/CN=kingdee.com" -days 5000 -out ca.crt
 
@@ -7,3 +8,13 @@ openssl genrsa -out server.key 2048
 openssl req -new -key server.key -subj "/CN=k8s-master" -config master_ssl.cnf -out server.csr
 
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -days 5000 -extensions v3_req -extfile master_ssl.cnf -out server.crt
+
+# kube-controller-manager
+openssl genrsa -out cs_client.key 2048
+openssl req -new -key cs_client.key -subj "/CN=k8s-kcm" -out cs_client.csr
+openssl x509 -req -in cs_client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -days 5000 -out cs_client.crt
+
+# kubelet
+openssl genrsa -out kubelet_client.key 2048
+openssl req -new -key kubelet_client.key -subj "/CN=k8s-node" -out kubelet_client.csr
+openssl x509 -req -in kubelet_client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -days 5000 -out kubelet_client.crt
