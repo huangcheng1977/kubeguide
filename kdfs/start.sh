4f35de484a9f872bae773fe92cd500fd7c25f4f0
carvin_chen <carvin_chen@kingdee.com>
2017-05-26 09:58:04 +0800

    knode && kserver work well

---
+++ kdfs/start.sh
@@ -0,0 +1,7 @@
+kubectl create -f knode-service.yaml
+kubectl create -f knode-controller.yaml
+kubectl create -f kserver-service.yaml
+kubectl create -f kserver-controller.yaml
+# kubectl create -f plugin-service.yaml
+# kubectl create -f plugin-controller.yaml
+
---
+++ kdfs/stop.sh
@@ -0,0 +1,7 @@
+kubectl delete -f knode-service.yaml
+kubectl delete -f knode-controller.yaml
+kubectl delete -f kserver-service.yaml
+kubectl delete -f kserver-controller.yaml
+# kubectl delete -f plugin-service.yaml
+# kubectl delete -f plugin-controller.yaml
+
